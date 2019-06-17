27b1000a3305b45d1a49b58ba633e54b7a67cc85
bhavin <bhavin.tagline@gmail.com>
2019-06-17 15:32:18 +0530

    Add user microposts

--- project-2/app/models/user.rb
+++ project-2/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 
-  has_many :microposts
+  has_many :microposts, dependent: :destroy
 
   # before_save { self.email = email.downcase }
   # before_save { email.downcase! }
@@ -71,6 +71,10 @@ class User < ActiveRecord::Base
 
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
+  end
+
+  def feed
+    Micropost.where("user_id = ?", id)
   end
 
   private
--- project-2/db/seeds.rb
+++ project-2/db/seeds.rb
@@ -32,4 +32,10 @@ User.create!(name:  "Example User",
                activated: true,
                activated_at: Time.zone.now)
 
+               users = User.order(:created_at).take(3)
+               5.times do
+                 content = Faker::Lorem.sentence(5)
+                 users.each { |user| user.microposts.create!(content: content) }
+               end
+
 end
